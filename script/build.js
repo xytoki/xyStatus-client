fa782b508def52965c5f9ab9bb79d8e0ebb93e06
xytoki <7547189+xytoki@users.noreply.github.com>
2020-03-22 17:06:21 +0800

    使用actions自动构建

--- script/build.js
+++ script/build.js
@@ -1,15 +1,13 @@
 const fs = require("fs")
 const path = require("path")
 const { exec } = require('pkg')
+const compressing = require('compressing');
 function p(s){
 	return path.resolve(__dirname+"/../"+s);
 }
 let packageJSON = JSON.parse(fs.readFileSync(p("package.json")));
 let targets=[
 	"node12-win-x86",
-	"node12-linux-x64",
-	"node12-alpine-x64",
-	"node12-linux-armv7"
 ];
 async function main(){
 	for(let target of targets){
@@ -23,6 +21,8 @@ async function main(){
 		await exec([p('/lib/index.js'), '--target', target, '--output', p(`/release/${target}/${output}`) ])
 		fs.writeFileSync(p(`/release/${target}/start.bat`),`@echo off\r\n${output} http://<username>:<password>@<ip> 10\r\npause`);
 		fs.writeFileSync(p(`/release/${target}/start.sh`),`#!/bin/sh\nchmod +x ${output}\n./${output} http://<username>:<password>@<ip> 10`);
+		console.log(`compressing /release/${target}.zip`);
+		await compressing.zip.compressDir(p(`/release/${target}`),p(`/release/${target}.zip`))
 	}
 }
 main();
